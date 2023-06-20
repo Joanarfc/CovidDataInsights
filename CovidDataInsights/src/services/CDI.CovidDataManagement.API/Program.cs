ede94bd02eefe93b49981ac4f44fb7e56184c5ff
joanarfc <joanarfcarv@gmail.com>
2023-06-20 10:38:53 -0300

    Changed environment configurations

--- CovidDataInsights/src/services/CDI.CovidDataManagement.API/Program.cs
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Program.cs
@@ -2,6 +2,11 @@ using CDI.CovidDataManagement.API.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
 builder.Services.AddApiConfiguration();
 builder.Services.AddSwaggerConfiguration();
 
