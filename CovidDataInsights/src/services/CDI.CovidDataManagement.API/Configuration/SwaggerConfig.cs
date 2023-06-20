742b99309bf2b1a5f46ca08be214a334ef66db1c
joanarfc <joanarfcarv@gmail.com>
2023-06-20 10:32:58 -0300

    Added Swagger configuration

---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace CDI.CovidDataManagement.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Covid-19 Data Management API",
+                    Description = "API for consuming, storing, and accessing COVID-19 data",
+                    Contact = new OpenApiContact() { Name = "Joana Carvalho", Email = "contact@covid-insights.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
+        }
+        public static void UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+        }
+    }
+}
--- CovidDataInsights/src/services/CDI.CovidDataManagement.API/Program.cs
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Program.cs
@@ -3,9 +3,11 @@ using CDI.CovidDataManagement.API.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApiConfiguration();
+builder.Services.AddSwaggerConfiguration();
 
 var app = builder.Build();
 
+app.UseSwaggerConfiguration();
 app.UseApiConfiguration();
 
 app.Run();
