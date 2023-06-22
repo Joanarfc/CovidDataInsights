de67f5397846ce214d705caaf8c98c36cfd0a816
joanarfc <joanarfcarv@gmail.com>
2023-06-22 14:13:58 -0300

    Added Entity class in CDI.Core project

---
+++ CovidDataInsights/src/building blocks/CDI.Core/DomainObjects/Entity.cs
@@ -0,0 +1,57 @@
+﻿
+namespace CDI.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        #region Comparisons
+
+        // Returns true if the current object is equal to the other object. Otherwise, false.
+        public override bool Equals(object? obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        // Returns true if the two objects are equal. Otherwise, false
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        // Returns true if the two objects are not equal. Otherwise, false
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        // Returns a hash code for the current object
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        // Returns a string that represents the current object
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+        #endregion
+    }
+}
