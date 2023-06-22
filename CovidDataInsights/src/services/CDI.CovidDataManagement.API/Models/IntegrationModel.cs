c5cef149079b22f6c5c69aeed2cf00f91fa122fe
joanarfc <joanarfcarv@gmail.com>
2023-06-22 14:07:07 -0300

    Added IntegrationModel class

---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Models/IntegrationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CDI.CovidDataManagement.API.Models
+{
+    public class IntegrationModel
+    {
+        public DateTime IntegrationTimestamp { get; set; }
+        public string? FileName { get; set; }
+        public int NumberOfRows { get; set; }
+        public int RowsIntegrated { get; set; }
+    }
+}
