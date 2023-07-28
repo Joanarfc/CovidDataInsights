8b4d815749625f2aecdd13fe59b7e363cc2343f8
joanarfc <joanarfcarv@gmail.com>
2023-07-28 10:00:11 -0300

    Added VaccinationDataModelToCsvMap and IntegrationModelCsvMap mappings

---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Data/Mappings/IntegrationModelCsvMap.cs
@@ -0,0 +1,16 @@
+﻿using CDI.CovidDataManagement.API.Models;
+using CsvHelper.Configuration;
+
+namespace CDI.CovidDataManagement.API.Data.Mappings
+{
+    public sealed class IntegrationModelCsvMap : ClassMap<IntegrationModel>
+    {
+        public IntegrationModelCsvMap()
+        {
+            Map(m => m.IntegrationTimestamp).Name("IntegrationTimestamp");
+            Map(m => m.FileName).Name("FileName");
+            Map(m => m.NumberOfRows).Name("NumberOfRows");
+            Map(m => m.RowsIntegrated).Name("RowsIntegrated");
+        }
+    }
+}
---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Data/Mappings/VaccinationDataModelToCsvMap.cs
@@ -0,0 +1,38 @@
+﻿using CDI.CovidDataManagement.API.Models;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace CDI.CovidDataManagement.API.Data.Mappings
+{
+    public sealed class VaccinationDataModelToCsvMap : ClassMap<VaccinationDataModel>
+    {
+        public VaccinationDataModelToCsvMap()
+        {
+            Map(m => m.Country).Name("COUNTRY");
+            Map(m => m.ISO3).Name("ISO3");
+            Map(m => m.WhoRegion).Name("WHO_REGION");
+            Map(m => m.DataSource).Name("DATA_SOURCE");
+            Map(m => m.DateUpdated).Name("DATE_UPDATED");
+            Map(m => m.TotalVaccinations).Name("TOTAL_VACCINATIONS")
+                .TypeConverterOption.NumberStyles(NumberStyles.AllowThousands)
+                .TypeConverterOption.NullValues(string.Empty);
+            Map(m => m.PersonsVaccinated_1Plus_Dose).Name("PERSONS_VACCINATED_1PLUS_DOSE")
+                .TypeConverterOption.NumberStyles(NumberStyles.AllowThousands)
+                .TypeConverterOption.NullValues(string.Empty); ;
+            Map(m => m.TotalVaccinations_Per100).Name("TOTAL_VACCINATIONS_PER100");
+            Map(m => m.PersonsVaccinated_1Plus_Dose_Per100).Name("PERSONS_VACCINATED_1PLUS_DOSE_PER100");
+            Map(m => m.PersonsLastDose).Name("PERSONS_LAST_DOSE").TypeConverterOption.NumberStyles(NumberStyles.AllowThousands)
+                .TypeConverterOption.NullValues(string.Empty); ;
+            Map(m => m.PersonsLastDosePer100).Name("PERSONS_LAST_DOSE_PER100");
+            Map(m => m.VaccinesUsed).Name("VACCINES_USED").Default(string.Empty);
+            Map(m => m.FirstVaccineDate).Name("FIRST_VACCINE_DATE")
+                .TypeConverterOption.Format("yyyy-MM-dd")
+                .TypeConverterOption.NullValues(string.Empty);
+            Map(m => m.NumberVaccinesTypesUsed).Name("NUMBER_VACCINES_TYPES_USED").Default(string.Empty);
+            Map(m => m.PersonsBoosterAddDose).Name("PERSONS_BOOSTER_ADD_DOSE")
+                .TypeConverterOption.NumberStyles(NumberStyles.AllowThousands)
+                .TypeConverterOption.NullValues(string.Empty); ;
+            Map(m => m.PersonsBoosterAddDose_Per100).Name("PERSONS_BOOSTER_ADD_DOSE_PER100");
+        }
+    }
+}
