e2ab2c1c5704076f33add515ab521d09adaac95b
joanarfc <joanarfcarv@gmail.com>
2023-08-08 14:30:59 -0300

    Added VaccinationMetaDataRepository and dependency injection configuration

--- CovidDataInsights/src/services/CDI.CovidDataManagement.API/Configuration/DependencyInjectionConfig.cs
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Configuration/DependencyInjectionConfig.cs
@@ -11,6 +11,7 @@ namespace CDI.CovidDataManagement.API.Configuration
             // Register repositories
             services.AddScoped<IFileIntegrationRepository, FileIntegrationRepository>();
             services.AddScoped<IVaccinationDataRepository, VaccinationDataRepository>();
+            services.AddScoped<IVaccinationMetaDataRepository, VaccinationMetaDataRepository>();
 
             // Register context
             services.AddScoped<ApplicationDbContext>();
---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Data/Repository/VaccinationMetaDataRepository.cs
@@ -0,0 +1,42 @@
+﻿using CDI.CovidDataManagement.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CDI.CovidDataManagement.API.Data.Repository
+{
+    public interface IVaccinationMetaDataRepository
+    {
+        Task AddVaccinationMetaDataRangeAsync(IEnumerable<VaccinationMetaDataModel> vaccinationMetaDataList);
+        Task<int> GetTotalCountAsync();
+    }
+    public class VaccinationMetaDataRepository : IVaccinationMetaDataRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VaccinationMetaDataRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<VaccinationMetaDataModel>> GetAllAsync()
+        {
+            return await (_context.VaccinationMetaData?.AsNoTracking().ToListAsync() ?? Task.FromResult(new List<VaccinationMetaDataModel>()));
+        }
+
+        public async Task<int> GetTotalCountAsync()
+        {
+            int totalCount = _context.VaccinationMetaData != null ? await _context.VaccinationMetaData.CountAsync() : 0;
+
+            return totalCount;
+        }
+
+        public async Task AddVaccinationMetaDataRangeAsync(IEnumerable<VaccinationMetaDataModel> vaccinationMetaDataList)
+        {
+            await (_context.VaccinationMetaData?.AddRangeAsync(vaccinationMetaDataList) ?? Task.CompletedTask);
+            await PersistData();
+        }
+
+        private async Task PersistData()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
