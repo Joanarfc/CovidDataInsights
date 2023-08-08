1e4547b86a22bb2c624de8f2de11b96a863debdc
joanarfc <joanarfcarv@gmail.com>
2023-08-08 12:31:31 -0300

    Added CsvDataController

---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Controllers/CsvDataController.cs
@@ -0,0 +1,30 @@
+﻿using CDI.CovidDataManagement.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CDI.CovidDataManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/csv-data")]
+    public class CsvDataController : ControllerBase
+    {
+        private readonly IFileIntegrationService _integrationService;
+        public CsvDataController(IFileIntegrationService integrationService)
+        {
+            _integrationService = integrationService;
+        }
+
+        [HttpPost("covid-data-integration")]
+        public async Task<IActionResult> IntegrateCsvData()
+        {
+            try
+            {
+                await _integrationService.IntegrateCsvDataAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while integrating CSV files: {ex.Message}");
+            }
+        }
+    }
+}
