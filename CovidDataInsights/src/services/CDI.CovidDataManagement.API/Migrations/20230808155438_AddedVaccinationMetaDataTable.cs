7912fa231ba3fd9a88d41387c95840fa3fe16b83
joanarfc <joanarfcarv@gmail.com>
2023-08-08 12:55:05 -0300

    Added migration for VaccinationMetaData

---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Migrations/20230808155438_AddedVaccinationMetaDataTable.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CDI.CovidDataManagement.API.Migrations
+{
+    public partial class AddedVaccinationMetaDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DateUpdated",
+                table: "VaccinationData",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.CreateTable(
+                name: "VaccinationMetaData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IntegrationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ISO3 = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    VaccineName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    CompanyName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    AuthorizationDate = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    StartDate = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    EndDate = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    Comment = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    DataSource = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VaccinationMetaData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VaccinationMetaData_IntegrationData_IntegrationId",
+                        column: x => x.IntegrationId,
+                        principalTable: "IntegrationData",
+                        principalColumn: "IntegrationId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VaccinationMetaData_IntegrationId",
+                table: "VaccinationMetaData",
+                column: "IntegrationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VaccinationMetaData");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateUpdated",
+                table: "VaccinationData",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+        }
+    }
+}
