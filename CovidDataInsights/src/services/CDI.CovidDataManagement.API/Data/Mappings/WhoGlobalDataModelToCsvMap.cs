2bc348ad9b53d1052e6b28d375f0cdb6f87705cc
joanarfc <joanarfcarv@gmail.com>
2023-08-08 15:25:49 -0300

    Added WhoGlobalDataModelToCsvMap mapping

---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Data/Mappings/WhoGlobalDataModelToCsvMap.cs
@@ -0,0 +1,20 @@
+﻿using CDI.CovidDataManagement.API.Models;
+using CsvHelper.Configuration;
+
+namespace CDI.CovidDataManagement.API.Data.Mappings
+{
+    public class WhoGlobalDataModelToCsvMap : ClassMap<WhoGlobalDataModel>
+    {
+        public WhoGlobalDataModelToCsvMap()
+        {
+            Map(m => m.DateReported).Name("Date_reported");
+            Map(m => m.CountryCode).Name("Country_code");
+            Map(m => m.Country).Name("Country");
+            Map(m => m.WhoRegion).Name("WHO_region");
+            Map(m => m.NewCases).Name("New_cases");
+            Map(m => m.CumulativeCases).Name("Cumulative_cases");
+            Map(m => m.NewDeaths).Name("New_deaths");
+            Map(m => m.CumulativeDeaths).Name("Cumulative_deaths");
+        }
+    }
+}
