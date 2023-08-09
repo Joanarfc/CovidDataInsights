139df92f5f2af7738472a350a58e7b94c6f28b2d
joanarfc <joanarfcarv@gmail.com>
2023-08-09 12:32:59 -0300

    Added WhoGlobalTableDataModelToCsvMap mapping

---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Data/Mappings/WhoGlobalTableDataModelToCsvMap.cs
@@ -0,0 +1,24 @@
+﻿using CDI.CovidDataManagement.API.Models;
+using CsvHelper.Configuration;
+
+namespace CDI.CovidDataManagement.API.Data.Mappings
+{
+    public class WhoGlobalTableDataModelToCsvMap : ClassMap<WhoGlobalTableDataModel>
+    {
+        public WhoGlobalTableDataModelToCsvMap()
+        {
+            Map(m => m.Name).Name("Name");
+            Map(m => m.WhoRegion).Name("WHO Region");
+            Map(m => m.CasesCumulativeTotal).Name("Cases - cumulative total");
+            Map(m => m.CasesCumulativeTotal_Per100000_Population).Name("Cases - cumulative total per 100000 population");
+            Map(m => m.CasesNewlyReportedInLast7Days).Name("Cases - newly reported in last 7 days");
+            Map(m => m.CasesNewlyReportedInLast7Days_Per100000_Population).Name("Cases - newly reported in last 7 days per 100000 population");
+            Map(m => m.CasesNewlyReportedInLast24Hours).Name("Cases - newly reported in last 24 hours");
+            Map(m => m.DeathsCumulativeTotal).Name("Deaths - cumulative total");
+            Map(m => m.DeathsCumulativeTotal_Per100000_Population).Name("Deaths - cumulative total per 100000 population");
+            Map(m => m.DeathsNewlyReportedInLast7Days).Name("Deaths - newly reported in last 7 days");
+            Map(m => m.DeathsNewlyReportedInLast7Days_Per100000_Population).Name("Deaths - newly reported in last 7 days per 100000 population");
+            Map(m => m.DeathsNewlyReportedInLast24Hours).Name("Deaths - newly reported in last 24 hours");
+        }
+    }
+}
