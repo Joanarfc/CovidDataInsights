46b3d45cca74e053807e8ea265909ef46ff51715
joanarfc <joanarfcarv@gmail.com>
2023-08-17 11:50:42 -0300

    Added GeoSpatial model

---
+++ CovidDataInsights/src/services/CDI.GeoSpatialDataLoader.API/Models/GeoSpatialModel.cs
@@ -0,0 +1,16 @@
+﻿using CDI.Core.DomainObjects;
+
+namespace CDI.GeoSpatialDataLoader.API.Models
+{
+    public class GeoSpatialModel : Entity
+    {
+        public string? Featurecla { get; set; }
+        public string? Sovereignt { get; set; }
+        public string? Type { get; set; }
+        public string? Admin { get; set; }
+        public string? NameLong { get; set; }
+        public string? FormalEN { get; set; }
+        public string? NameEN { get; set; }
+        public string? Coordinates { get; set; }
+    }
+}
