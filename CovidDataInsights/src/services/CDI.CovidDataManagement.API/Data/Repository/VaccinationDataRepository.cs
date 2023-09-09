06b2fa63666efaa7d1f776ece8ce5d16e69e8556
joanarfc <joanarfcarv@gmail.com>
2023-09-09 08:54:12 -0300

    Added methods GetTotalVaccineDosesAsync, GetTotalPersonsVaccinatedAtLeastOneDoseAsync and GetTotalPersonsVaccinatedWithCompletePrimarySeriesAsync in VaccinationDataRepository to get the vaccination data from the database

--- CovidDataInsights/src/services/CDI.CovidDataManagement.API/Data/Repository/VaccinationDataRepository.cs
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/Data/Repository/VaccinationDataRepository.cs
@@ -1,19 +1,28 @@
-﻿using CDI.CovidDataManagement.API.Models;
+﻿#nullable disable
+using CDI.CovidDataManagement.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CDI.CovidDataManagement.API.Data.Repository
 {
     public interface IVaccinationDataRepository
     {
+        Task AddVaccinationDataRangeAsync(IEnumerable<VaccinationDataModel> vaccinationDataList);
         Task<List<VaccinationDataModel>> GetAllAsync();
         Task<int> GetTotalCountAsync();
-        Task AddVaccinationDataRangeAsync(IEnumerable<VaccinationDataModel> vaccinationDataList);
+        Task<long?> GetTotalVaccineDosesAsync(string filename, string country = null);
+        Task<long?> GetTotalPersonsVaccinatedAtLeastOneDoseAsync(string filename, string country = null);
+        Task<long?> GetTotalPersonsVaccinatedWithCompletePrimarySeriesAsync(string filename, string country = null);
     }
     public class VaccinationDataRepository : Repository<VaccinationDataModel>, IVaccinationDataRepository
     {
         public VaccinationDataRepository(ApplicationDbContext context) : base(context)
-        {   }
+        { }
 
+        public async Task AddVaccinationDataRangeAsync(IEnumerable<VaccinationDataModel> vaccinationDataList)
+        {
+            await (_context.VaccinationData?.AddRangeAsync(vaccinationDataList) ?? Task.CompletedTask);
+            await PersistData();
+        }
         public async Task<List<VaccinationDataModel>> GetAllAsync()
         {
             return await (_context.VaccinationData?.AsNoTracking().ToListAsync() ?? Task.FromResult(new List<VaccinationDataModel>()));
@@ -26,10 +35,51 @@ namespace CDI.CovidDataManagement.API.Data.Repository
             return totalCount;
         }
 
-        public async Task AddVaccinationDataRangeAsync(IEnumerable<VaccinationDataModel> vaccinationDataList)
+        public async Task<long?> GetTotalVaccineDosesAsync(string filename, string country = null)
         {
-            await (_context.VaccinationData?.AddRangeAsync(vaccinationDataList) ?? Task.CompletedTask);
-            await PersistData();
+            var query = _context.IntegrationData
+                .Where(id => id.FileName == filename)
+                .SelectMany(id => id.VaccinationData);
+
+            if (!string.IsNullOrEmpty(country))
+            {
+                query = query.Where(vd => vd.Country == country);
+            }
+
+            var totalVaccineDoses = await query.SumAsync(vd => vd.TotalVaccinations);
+
+            return totalVaccineDoses;
+        }
+
+        public async Task<long?> GetTotalPersonsVaccinatedAtLeastOneDoseAsync(string filename, string country = null)
+        {
+            var query = _context.IntegrationData
+                .Where(id => id.FileName == filename)
+                .SelectMany(id => id.VaccinationData);
+
+            if (!string.IsNullOrEmpty(country))
+            {
+                query = query.Where(vd => vd.Country == country);
+            }
+
+            var totalPersonsVaccinatedAtLeastOneDose = await query.SumAsync(vd => vd.PersonsVaccinated_1Plus_Dose);
+
+            return totalPersonsVaccinatedAtLeastOneDose;
+        }
+        public async Task<long?> GetTotalPersonsVaccinatedWithCompletePrimarySeriesAsync(string filename, string country = null)
+        {
+            var query = _context.IntegrationData
+                .Where(id => id.FileName == filename)
+                .SelectMany(id => id.VaccinationData);
+
+            if (!string.IsNullOrEmpty(country))
+            {
+                query = query.Where(vd => vd.Country == country);
+            }
+
+            var totalPersonsVaccinatedWithCompletePrimarySeries = await query.SumAsync(vd => vd.PersonsLastDose);
+
+            return totalPersonsVaccinatedWithCompletePrimarySeries;
         }
     }
 }
