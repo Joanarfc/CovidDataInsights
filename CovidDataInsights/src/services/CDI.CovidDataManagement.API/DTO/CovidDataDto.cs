ac0d7c72fd8393f20a818bf323915d6e29715127
joanarfc <joanarfcarv@gmail.com>
2023-09-09 11:19:15 -0300

    Added NewCasesLast7Days, CumulativeCases, NewDeathsLast7Days and CumulativeDeaths to CovidDataDto

--- CovidDataInsights/src/services/CDI.CovidDataManagement.API/DTO/CovidDataDto.cs
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/DTO/CovidDataDto.cs
@@ -6,5 +6,9 @@
         public long? TotalVaccineDoses { get; set; }
         public long? TotalPersonsVaccinatedAtLeastOneDose { get; set; }
         public long? TotalPersonsVaccinatedWithCompletePrimarySeries { get; set; }
+        public int? NewCasesLast7Days { get; set; }
+        public long? CumulativeCases { get; set; }
+        public int? NewDeathsLast7Days { get; set; }
+        public long? CumulativeDeaths { get; set; }
     }
 }
