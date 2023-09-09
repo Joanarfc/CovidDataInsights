677e60c95b279d237e8f0e04be1d77fb801469f1
joanarfc <joanarfcarv@gmail.com>
2023-09-09 08:55:30 -0300

    Added CovidDataDto class to CDI.CovidDataManagement.API project

---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/DTO/CovidDataDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CDI.CovidDataManagement.API.DTO
+{
+    public class CovidDataDto
+    {
+        public string? Region { get; set; }
+        public long? TotalVaccineDoses { get; set; }
+        public long? TotalPersonsVaccinatedAtLeastOneDose { get; set; }
+        public long? TotalPersonsVaccinatedWithCompletePrimarySeries { get; set; }
+    }
+}
