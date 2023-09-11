4ee7a1ffe32758afbe6f975fe4d749242c3a9cd4
joanarfc <joanarfcarv@gmail.com>
2023-09-11 16:06:42 -0300

    Added APIs urls to appsettings.Development.json and added AppSettings.cs

---
+++ CovidDataInsights/src/web/CDI.CovidApp.MVC/Extensions/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace CDI.CovidApp.MVC.Extensions
+{
+    public class AppSettings
+    {
+        public string? CovidDataUrl { get; set; }
+        public string? GeoDataUrl { get; set; }
+    }
+}
