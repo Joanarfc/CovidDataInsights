5ed5f650333120ca350fb67c1597d9a13a859ec7
joanarfc <joanarfcarv@gmail.com>
2023-09-12 15:43:17 -0300

    Added models ResponseResult and ResponseErrorMessages

---
+++ CovidDataInsights/src/web/CDI.CovidApp.MVC/Models/ResponseResultViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace CDI.CovidApp.MVC.Models
+{
+    public class ResponseResultViewModel
+    {
+        public ResponseResultViewModel()
+        {
+            Errors = new ResponseErrorMessages();
+        }
+        public string? Title { get; set; }
+        public int Status { get; set; }
+        public ResponseErrorMessages Errors { get; set; }
+    }
+    public class ResponseErrorMessages
+    {
+        public ResponseErrorMessages()
+        {
+            Messages = new List<string>();
+        }
+        public List<string> Messages { get; set; }
+    }
+}
