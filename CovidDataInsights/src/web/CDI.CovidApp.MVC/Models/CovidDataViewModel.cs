58f7bdba5b1746498eebb3da33003135cfb813a9
joanarfc <joanarfcarv@gmail.com>
2023-09-12 14:04:51 -0300

    Added CovidDataViewModel to the Web app

---
+++ CovidDataInsights/src/web/CDI.CovidApp.MVC/Models/CovidDataViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CDI.CovidApp.MVC.Models
+{
+    public class CovidDataViewModel
+    {
+        public string? Region { get; set; }
+        public long? TotalVaccineDoses { get; set; }
+        public long? TotalPersonsVaccinatedAtLeastOneDose { get; set; }
+        public long? TotalPersonsVaccinatedWithCompletePrimarySeries { get; set; }
+        public int? NewCasesLast7Days { get; set; }
+        public long? CumulativeCases { get; set; }
+        public int? NewDeathsLast7Days { get; set; }
+        public long? CumulativeDeaths { get; set; }
+    }
+}
