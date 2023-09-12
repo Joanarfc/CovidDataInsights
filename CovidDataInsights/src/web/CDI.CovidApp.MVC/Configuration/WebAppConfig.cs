e5eea0b4d43d5d1b0cc444c68875a87825a30dd1
joanarfc <joanarfcarv@gmail.com>
2023-09-12 13:55:50 -0300

    Added appsettings configuration and moved Program.cs configurations to a separate file named WebAppConfig

---
+++ CovidDataInsights/src/web/CDI.CovidApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,33 @@
+﻿namespace CDI.CovidApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static void AddMvcConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews();
+
+        }
+        public static void UseMvcConfiguration(this IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
