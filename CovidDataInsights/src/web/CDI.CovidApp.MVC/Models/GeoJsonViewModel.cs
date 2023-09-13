0edc510f6f3f5d6c5ac27b908c2c99e99665698e
joanarfc <joanarfcarv@gmail.com>
2023-09-13 14:18:29 -0300

    Added GeoJsonViewModel

---
+++ CovidDataInsights/src/web/CDI.CovidApp.MVC/Models/GeoJsonViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CDI.CovidApp.MVC.Models
+{
+    public class GeoJsonViewModel
+    {
+        [JsonPropertyName("featurecla")]
+        public string? Featurecla { get; set; }
+        [JsonPropertyName("sovereignt")]
+        public string? Sovereignt { get; set; }
+        [JsonPropertyName("type")]
+        public string? Type { get; set; }
+        [JsonPropertyName("admin")]
+        public string? Admin { get; set; }
+        [JsonPropertyName("nameLong")]
+        public string? NameLong { get; set; }
+        [JsonPropertyName("formalEN")]
+        public string? FormalEN { get; set; }
+        [JsonPropertyName("nameEN")]
+        public string? NameEN { get; set; }
+        [JsonPropertyName("coordinates")]
+        public string? Coordinates { get; set; }
+    }
+}
