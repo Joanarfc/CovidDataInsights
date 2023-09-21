c3cb16566d569e50af956d71cfd73cf681366bb5
joanarfc <36134456+Joanarfc@users.noreply.github.com>
2023-09-21 19:37:26 -0300

    Added GetCovidGeojsonDataAsync action in CovidDataController

--- CovidDataInsights/src/web/CDI.CovidApp.MVC/Controllers/CovidDataController.cs
+++ CovidDataInsights/src/web/CDI.CovidApp.MVC/Controllers/CovidDataController.cs
@@ -6,10 +6,12 @@ namespace CDI.CovidApp.MVC.Controllers
     public class CovidDataController : Controller
     {
         private readonly ICovidDataService _covidDataService;
+        private readonly ICovidWithGeoJsonDataService _covidWithGeoJsonDataService;
 
-        public CovidDataController(ICovidDataService covidDataService)
+        public CovidDataController(ICovidDataService covidDataService, ICovidWithGeoJsonDataService covidWithGeoJsonDataService)
         {
             _covidDataService = covidDataService;
+            _covidWithGeoJsonDataService = covidWithGeoJsonDataService;
         }
         [HttpGet]
         [Route("")]
@@ -26,6 +28,14 @@ namespace CDI.CovidApp.MVC.Controllers
         public async Task<IActionResult> GetAllTotalCovidDataAsync()
         {
             var allCovidData = await _covidDataService.GetAllTotalCovidDataAsync();
+
+            return Ok(allCovidData);
+        }
+        [HttpGet]
+        [Route("covid-geojson-data")]
+        public async Task<IActionResult> GetCovidGeojsonDataAsync()
+        {
+            var allCovidData = await _covidWithGeoJsonDataService.GetCovidWithGeoJsonDataAsync();
 
             return Ok(allCovidData);
         }
