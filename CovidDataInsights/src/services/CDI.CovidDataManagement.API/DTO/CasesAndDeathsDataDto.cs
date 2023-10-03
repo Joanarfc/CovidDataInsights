6640fbe6f258901a02a28320d2ec6dab014af389
joanarfc <36134456+Joanarfc@users.noreply.github.com>
2023-10-03 14:05:21 -0300

    Added CasesAndDeathsDataDto to CDI.CovidDataManagement.API

---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/DTO/CasesAndDeathsDataDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CDI.CovidDataManagement.API.DTO
+{
+    public class CasesAndDeathsDataDto
+    {
+        public string? Region { get; set; }
+        public int? NewCasesLast7Days { get; set; }
+        public long? CumulativeCases { get; set; }
+        public int? NewDeathsLast7Days { get; set; }
+        public long? CumulativeDeaths { get; set; }
+    }
+}
