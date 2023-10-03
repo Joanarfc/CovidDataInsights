1902d00e9530592070f81167554aeccc14af92e4
joanarfc <36134456+Joanarfc@users.noreply.github.com>
2023-10-03 13:58:21 -0300

    Added VaccinationDataDto to CDI.CovidDataManagement.API

---
+++ CovidDataInsights/src/services/CDI.CovidDataManagement.API/DTO/VaccinationDataDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CDI.CovidDataManagement.API.DTO
+{
+    public class VaccinationDataDto
+    {
+        public string? Region { get; set; }
+        public long? TotalVaccineDoses { get; set; }
+        public long? PersonsVaccinatedAtLeastOneDose { get; set; }
+        public long? PersonsVaccinatedWithCompletePrimarySeries { get; set; }
+    }
+}
